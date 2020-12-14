e120683db825f1af21edc5598dea7425673d96f2
ren <ren80001@gmail.com>
2020-12-15 03:02:22 +0900

    jsの追加と読み込み

---
+++ django/static/admin/js/load.js
@@ -0,0 +1,15 @@
+$('.Likes-Icon').on('click', function() {
+    let $btn = $(this);
+    // Likeアイコンがonクラス持っていたら
+    if ($btn.hasClass('on')) {
+
+      $btn.removeClass('on');
+      $btn.removeClass("HeartAnimation");
+      $btn.css("background-position","left");
+
+    } else {
+      $btn.addClass('on');
+      $btn.addClass("HeartAnimation");
+
+    }
+});
