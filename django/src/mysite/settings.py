c55e273da4117523ff22a616e27201c5358a5d5f
ren80001 <52253850+ren80001@users.noreply.github.com>
2020-12-18 18:16:13 +0900

    Update settings.py

--- django/src/mysite/settings.py
+++ django/src/mysite/settings.py
@@ -87,11 +87,8 @@ DATABASES = {
         'NAME': 'userdata',
         'USER': 'admin',
         'PASSWORD': 'ren11261126',
-        'HOST': 'database-1.czhr67wuwire.ap-northeast-1.rds.amazonaws.com',
+        'HOST': 'freelanze-db.czhr67wuwire.ap-northeast-1.rds.amazonaws.com',
         'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
     }
 }
 
