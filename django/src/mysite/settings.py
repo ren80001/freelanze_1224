efbac0c210d8691f8cd15fed57371e80f805a615
ren80001 <52253850+ren80001@users.noreply.github.com>
2020-12-18 15:17:49 +0900

    Update settings.py

--- django/src/mysite/settings.py
+++ django/src/mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9veo^crfmr2era$=vmh5lq9=%xc1qj@!vu59&zoq!r1_(2r*29'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
